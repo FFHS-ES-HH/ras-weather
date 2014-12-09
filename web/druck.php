c5ce454bacea2928d254ee84dab24eaa58cb00bb
edelweiss0 <andi_hasler@gmx.ch>
2014-12-09 23:47:21 +0100

    Web-Dateien und Dokumentation angepasst

--- web/druck.php
+++ web/druck.php
@@ -1,4 +1,29 @@
 <?php
+	/*
+	 * LICENSE
+	 *
+	 * Copyright (c) 2014, Andreas Hasler (ah), andreas.hasler@students.ffhs.ch
+	 *
+	 * druck.php is free software copyrighted by Andreas Hasler.
+	 *
+	 * This program is free software: you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation, either version 3 of the License, or
+	 * any later version.
+	 *
+	 * This program is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 * GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public License
+	 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 *
+	 * This program comes with ABSOLUTELY NO WARRANTY.
+	 * This is free software, and you are welcome to redistribute it
+	 * under certain conditions.
+	 */
+
 	// Die Klasse für den Datenbank-Zugriff wird benötigt
 	require('classes/databasehandler.php');
 ?>
